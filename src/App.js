0f35f3136667dff0bb7ae5aa1bfac9dfc6297ee1
MTVerso <96653995+MTVerso@users.noreply.github.com>
2022-11-14 17:15:33 +0000

    o

--- src/App.js
+++ src/App.js
@@ -17,24 +17,27 @@ export default function App() {
       <h1>sec felx box</h1>  
       <section class='flex'>
         <div>
-           <img src='' width='200px' height='300px' alt='imagem'/>
+           <img src='https://github.com/MTVerso/brservapp/blob/main/src/img2internal/ft1.JPG?raw=true' width='200px' height='300px' alt='imagem'/>
            <p>imagem 1</p>
         </div> 
         <div>
-           <img src=''  width='200px' height='300px'alt='imagem'/>
+           <img src='https://github.com/MTVerso/brservapp/blob/main/src/img2internal/ft2.JPG?raw=true'  width='200px' height='300px'alt='imagem'/>
            <p>imagem 2</p>
         </div>      <div>
-           <img src='e' width='200px' height='300px' alt='imagem'/>
+           <img src='https://github.com/MTVerso/brservapp/blob/main/src/img2internal/ft3.jpg?raw=true' width='200px' height='300px' alt='imagem'/>
            <p>imagem 3</p>
         </div>      <div>
-           <img src='e'  width='200px' height='300px'alt='imagem'/>
+           <img src='https://github.com/MTVerso/brservapp/blob/main/src/img2internal/ft4.JPG?raw=true'  width='200px' height='300px'alt='imagem'/>
            <p>imagem 4</p>
         </div>      <div>
-           <img src='e' width='200px' height='300px' alt='imagem'/>
+           <img src='https://github.com/MTVerso/brservapp/blob/main/src/img2internal/ft6.JPG?raw=true' width='200px' height='300px' alt='imagem'/>
            <p>imagem 5</p>
         </div>      <div>
-           <img src='e' width='200px' height='300px' alt='imagem'/>
+           <img src='https://github.com/MTVerso/brservapp/blob/main/src/img2internal/ft6.JPG?raw=true' width='200px' height='300px' alt='imagem'/>
            <p>imagem 6</p>
+           </div>      <div>
+           <img src='https://github.com/MTVerso/brservapp/blob/main/src/img2internal/ft7.JPG?raw=true' width='200px' height='300px' alt='imagem'/>
+           <p>imagem 7</p>
         </div> 
       </section>
      </div>
