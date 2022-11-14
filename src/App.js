569dcc40379779eb6d4886352b3abe8cd8e8d4ff
MTVerso <96653995+MTVerso@users.noreply.github.com>
2022-11-14 14:05:54 +0000

    n

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div>
+      <header class="header">
+        <a href="/">logo</a>
+        <nav>
+          <ul class="menu">
+            <li> <a href="/">sobre</a> </li>
+            <li> <a href="/">infromações</a> </li>
+            <li> <a href="/">contatos</a> </li>
+          </ul>
+        </nav>
+      </header>
+      <h1>sec felx box</h1>  
+      <section>
+<div>
+ 
+
+</div>
+
+      </section>
+     </div>
+  );
+}
