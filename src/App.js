2444536c4f341060c59d9d1e7eda98b14eb897f8
MTVerso <96653995+MTVerso@users.noreply.github.com>
2022-11-14 18:35:30 +0000

    link

--- src/App.js
+++ src/App.js
@@ -40,6 +40,36 @@ export default function App() {
            <p></p>
         </div> 
       </section>
+      <div>
+<br/><br/>
+      <p> A empresa Brasil Service ltda me fundada em data de abertura
+03/04/2019 e com as seguintes atividades cadastradas prestadas. Construção de 
+estações e redes de distribuição de energia elétrica. Construção de estações e 
+redes de telecomunicações. Outras obras de engenharia civil não especificadas anteriormente. Localizada com escritório sede na rua Laurinda pereira nascimento, graúna, Cariacica, ES. 
+Romildo Effgen responsável geral pela empresa, sobre Confiança
+
+A confiança é a crença que temos no caráter, nos bons valores e na lealdade de alguém.
+Uma pessoa confiável é aquela que transmite honestidade e apresenta bom caráter, ajudando o outro a acreditar em suas boas atitudes e escolhas.
+Há também a autoconfiança, que é a capacidade de acreditar no próprio potencial.
+
+A Criatividade
+A criatividade é a característica de criar ou inovar algo, seja no lado profissional ou pessoal.
+Uma pessoa criativa costuma ser muito valorizada no ambiente de trabalho, devido à sua originalidade e capacidade de ter pontos de vista e ideias diferentes.
+
+Entretanto a Tolerância
+A tolerância é a capacidade de perdoar certos erros e aceitar opiniões ou comportamentos que difiram dos seus.
+Quem é tolerante aceita que as pessoas são diferentes umas das outras e respeita essas diferenças, seja de pensamentos, ideias, opiniões ou comportamentos.
+Além do mais, também tem consciência de que os acontecimentos não estão sob seu controle, por isso consegue administrar as expectativas ao longo da vida.
+</p>
+<br></br>
+      </div>
+
+      <div>
+         
+        <ab><a href='https://solucoes.receita.fazenda.gov.br/Servicos/cnpjreva/Cnpjreva_Solicitacao.asp?cnpj=33246094000132'>CNPJ receita.gov</a></ab>
+         
+         </div>
+
      </div>
   );
 }
