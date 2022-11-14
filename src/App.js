1f3a0a03139e1e043904f4db2cf6243cb5ee4a30
MTVerso <96653995+MTVerso@users.noreply.github.com>
2022-11-14 17:48:27 +0000

    o

--- src/App.js
+++ src/App.js
@@ -4,40 +4,40 @@ export default function App() {
   return (
     <div>
       <header class="header">
-        <img src="https://github.com/MTVerso/brservapp/blob/main/src/img/logo.png?raw=true"/>
-        <a href="/">logo</a>
+        <img src="https://github.com/MTVerso/brservapp/blob/main/src/img/logo.png?raw=true" width='90px' height='65px'/>
+        <br/>
+        <h1><a><a href="/">Brasil Service</a></a></h1>
         <nav>
           <ul class="menu">
-            <li> <a href="/">sobre</a> </li>
-            <li> <a href="/">infromações</a> </li>
-            <li> <a href="/">contatos</a> </li>
+            <li> <a href="/">Curiosidades e Dicas</a> </li>
+            <li> <a href="/">Contatos</a> </li>
           </ul>
         </nav>
       </header>
-      <h1>sec felx box</h1>  
+      <h1>Brasil Service Ltda ME</h1>  
       <section class='flex'>
         <div>
            <img src='https://github.com/MTVerso/brservapp/blob/main/src/img2internal/ft1.JPG?raw=true' width='200px' height='300px' alt='imagem'/>
-           <p>imagem 1</p>
+           <p></p>
         </div> 
         <div>
            <img src='https://github.com/MTVerso/brservapp/blob/main/src/img2internal/ft2.JPG?raw=true'  width='200px' height='300px'alt='imagem'/>
-           <p>imagem 2</p>
+           <p></p>
         </div>      <div>
            <img src='https://github.com/MTVerso/brservapp/blob/main/src/img2internal/ft3.jpg?raw=true' width='200px' height='300px' alt='imagem'/>
-           <p>imagem 3</p>
+           <p></p>
         </div>      <div>
            <img src='https://github.com/MTVerso/brservapp/blob/main/src/img2internal/ft4.JPG?raw=true'  width='200px' height='300px'alt='imagem'/>
-           <p>imagem 4</p>
+           <p></p>
         </div>      <div>
            <img src='https://github.com/MTVerso/brservapp/blob/main/src/img2internal/ft6.JPG?raw=true' width='200px' height='300px' alt='imagem'/>
-           <p>imagem 5</p>
+           <p></p>
         </div>      <div>
-           <img src='https://github.com/MTVerso/brservapp/blob/main/src/img2internal/ft6.JPG?raw=true' width='200px' height='300px' alt='imagem'/>
-           <p>imagem 6</p>
+           <img src='https://github.com/MTVerso/brservapp/blob/main/src/img2internal/ft5.JPG?raw=true' width='200px' height='300px' alt='imagem'/>
+           <p></p>
            </div>      <div>
            <img src='https://github.com/MTVerso/brservapp/blob/main/src/img2internal/ft7.JPG?raw=true' width='200px' height='300px' alt='imagem'/>
-           <p>imagem 7</p>
+           <p></p>
         </div> 
       </section>
      </div>
