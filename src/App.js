4db6b6ba84d31dff6f48910edabb01f48ac95318
MTVerso <96653995+MTVerso@users.noreply.github.com>
2022-11-14 17:12:24 +0000

    novas

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ export default function App() {
   return (
     <div>
       <header class="header">
-        <img src="./img/logo.png"/>
+        <img src="https://github.com/MTVerso/brservapp/blob/main/src/img/logo.png?raw=true"/>
         <a href="/">logo</a>
         <nav>
           <ul class="menu">
@@ -17,23 +17,23 @@ export default function App() {
       <h1>sec felx box</h1>  
       <section class='flex'>
         <div>
-           <img src='https://github.com/MTVerso/brservapp/blob/main/src/img/Imaget.jpg?raw=true' width='200px' height='300px' alt='imagem'/>
+           <img src='' width='200px' height='300px' alt='imagem'/>
            <p>imagem 1</p>
         </div> 
         <div>
-           <img src='e' alt='imagem'/>
+           <img src=''  width='200px' height='300px'alt='imagem'/>
            <p>imagem 2</p>
         </div>      <div>
-           <img src='e' alt='imagem'/>
+           <img src='e' width='200px' height='300px' alt='imagem'/>
            <p>imagem 3</p>
         </div>      <div>
-           <img src='e' alt='imagem'/>
+           <img src='e'  width='200px' height='300px'alt='imagem'/>
            <p>imagem 4</p>
         </div>      <div>
-           <img src='e' alt='imagem'/>
+           <img src='e' width='200px' height='300px' alt='imagem'/>
            <p>imagem 5</p>
         </div>      <div>
-           <img src='e' alt='imagem'/>
+           <img src='e' width='200px' height='300px' alt='imagem'/>
            <p>imagem 6</p>
         </div> 
       </section>
